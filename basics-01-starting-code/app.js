1ae39c4c7c3637df87788c96cf06c824e53a0446
Mesfer <mesfer.vip@gmail.com>
2020-10-13 14:56:05 +0300

    second commit

--- basics-01-starting-code/app.js
+++ basics-01-starting-code/app.js
@@ -1,3 +1,22 @@
 const app = Vue.createApp({
-	
-})
+
+data(){
+	return{
+		courseGoalA: 'Finsh this course and learn Vue',
+		courseGoalB: 'Master of Vue',
+		VueLink: 'https://vuejs.org/'
+	};
+},
+methods:{
+	outputGoal(){
+		const randomNumber = Math.random(); //random between 0 & 1
+		if (randomNumber < 0.5){
+			return this.courseGoalA;
+		}
+		else{
+			return this.courseGoalB;
+		}
+	}
+}
+});
+app.mount('#user-goal');
